b4cc14832f4cedf15bc020e5cdd714f49225306a
FernandoUtzig <fernandoutzig@hotmail.com>
2021-10-22 16:42:39 -0300

    gerar o arquivo binario

---
+++ TrabalhoT2-Light-FS/FAT8/fat8.cpp
@@ -0,0 +1,416 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <time.h>
+#include <math.h>
+#include <cstdlib>
+
+using namespace std;
+
+#define BLOCK_SIZE 64
+#define BLOCK_COUNT 256
+#define DISK_FILENAME "HD.DH.txt"
+
+class HardDrive
+{
+private:
+    string FillWithWhiteSpace(int number)
+    {
+        string output = "";
+        for (int i=0; i<number; i++)
+        {
+            output += " ";
+        }
+        return output;
+    }
+
+public:
+
+    /**
+     * Read all memory into string, then put
+     * right block of memory into tampLecture
+     * @param numeroBlock position of the bloc to read
+     * @param tampLecture string to contain the block value
+     */
+    void ReadBlock(int numeroBlock, string &tampLecture)
+    {
+        ifstream file(DISK_FILENAME);
+        string memory = "";
+        tampLecture = "";
+
+        while (!file.eof())
+        {
+            string temp;
+            file >> temp;
+            memory += temp;
+        }
+        memory += FillWithWhiteSpace(BLOCK_SIZE - memory.length());
+
+        for (int i=0; i < memory.length(); i++)
+        {
+            if (i > BLOCK_SIZE * numeroBlock)
+            {
+                for (int j=i; j < i + BLOCK_SIZE && j+i < memory.length(); j++)
+                {
+                    tampLecture += memory[j];
+                }
+                break;
+            }
+        }
+
+        file.close();
+    }
+
+    /**
+     * read all memory blocks until given block position,
+     * then write to disk content from tampEcriture, then
+     * continue reading the rest of the memory. After that,
+     * write all memory to disk.
+     * @param numeroBlock  position of the block to write
+     * @param tampEcriture content to write in block
+     */
+    void WriteBlock(int numeroBlock, string tampEcriture)
+    {
+        ifstream getFile(DISK_FILENAME);
+        string disk[BLOCK_COUNT];
+
+        for (int i=0; i < BLOCK_COUNT; i++)
+        {
+            if (numeroBlock != i)
+            {
+                if (!getFile.eof())
+                {
+                    getFile >> disk[i];
+                }
+                else
+                {
+                    disk[i] = " ";
+                }
+            }
+            else
+            {
+                disk[i] = tampEcriture;
+            }
+        }
+
+        ofstream file(DISK_FILENAME);
+        for (int i=0; i < BLOCK_COUNT; i++)
+        {
+            file << disk[i] << endl;
+        }
+
+        file.close();
+        getFile.close();
+    }
+
+    /*
+     * Get the first available empty memory space.
+     */
+    int GetFirstAvailableMemorySpace()
+    {
+        ifstream file(DISK_FILENAME);
+
+        for (int i=1; 1; i++)
+        {
+            if (i == BLOCK_COUNT)
+                return -1;
+            if (file.eof())
+                return i;
+            string out;
+            file >> out;
+            char test = (char)out[0];
+            if (test == '\0')
+                return i;
+        }
+
+        file.close();
+    }
+
+    /**
+    * Returns the space left in block in characters count
+    * @param  block the block to get available space from
+    * @return       (int)space available if block found, -1 otherwise
+    */
+    int spaceLeftInBlock(int block)
+    {
+        ifstream file(DISK_FILENAME);
+        string line = "";
+        for (int i = 0; i <= block; i++)
+        {
+            file >> line;
+            if (i == block)
+            {
+                return BLOCK_SIZE - line.length();
+            }
+        }
+        return -1;
+    }
+};
+
+class OS
+{
+
+    HardDrive *hd;
+    int FAT [BLOCK_COUNT];
+    map<string, int> files; //corresponding filename and starting block in FAT
+
+public:
+    OS()
+    {
+        hd = new HardDrive();
+        for (int i = 0; i < BLOCK_COUNT; i++)
+        {
+            FAT[i] = 0;
+        }
+    }
+    ~OS()
+    {
+        delete hd;
+    }
+
+    /**
+    * adds a new file manually to the files dictionnary
+    * @param filename the filename of the added file, to which is mapped
+    * its starting block in FAT
+    */
+    void addFile(string filename)
+    {
+        files.insert(std::pair<string, int>(filename, hd->GetFirstAvailableMemorySpace()));
+    }
+
+    /**
+    * Read from nomFichier nombreCaracteres characters from position,
+    * and writes it to tampLecture that's passed as reference
+    * @param nomFichier       The filename to read from
+    * @param position         starting position of reading
+    * @param nombreCaracteres number of chars to read
+    * @param tampLecture      the buffer where to write the read chars
+    */
+    void read(string nomFichier, int position, int nombreCaracteres, string &tampLecture)
+    {
+        int index = files[nomFichier];
+        string output = "";
+        while (index != 0)
+        {
+            hd->ReadBlock(position, tampLecture);
+            output += tampLecture;
+            index = FAT[index];
+        }
+        tampLecture = output;
+    }
+
+
+    /**
+    * Writes the content of tampLecture to a given
+    * file from position for nombreCaracteres
+    * @param nomFichier       The filename where to append the content
+    * @param position         The character position where to start writing
+    * @param nombreCaracteres The number of characters to write (aka tampLecture.length())
+    * @param tampLecture      The content to write to file
+    */
+    void write(string nomFichier, int position, int nombreCaracteres, string tampLecture)
+    {
+        int block;
+        string overwritten = "";
+        //get index of file, if file does not exist,
+        //index = 0, so give it a correct index
+        int index = files[nomFichier];
+
+        //if file does not exist in dictionnary
+        if (index == 0)
+        {
+            block = hd->GetFirstAvailableMemorySpace(); //returns a block between 0 and 255 empty in FAT
+            if (block == -1)
+            {
+                cout << "No memory available in hard drive!" << endl;
+                return;
+            }
+            files[nomFichier] = block;
+        }
+        //if file exists in dictionnary
+        else
+        {
+            //get starting block from where to write
+            int blockNumber = floor(position / BLOCK_SIZE);
+            //then get to this block by going through FAT
+            for (int i = 0; i < blockNumber; i++)
+            {
+                blockNumber = FAT[blockNumber];
+            }
+            block = blockNumber;
+        }
+
+        //calculates the char where to start writing into block (from 0 to 63)
+        int offset = position < BLOCK_SIZE ? position : position % BLOCK_SIZE;
+
+        //calculates the number of characters that have to be written in first block ( < 64 )
+        int nbCharsInFirstBlock = BLOCK_SIZE - offset;
+
+        //calculates the number of blocks to write (number of iterations in FAT)
+        int blocksToWrite = 1 + ceil((nombreCaracteres - nbCharsInFirstBlock) / BLOCK_SIZE);
+
+        //gets the first char block to write to incomplete first block
+        string temp = tampLecture.substr(0, nbCharsInFirstBlock);
+        //then change the position to which to write into file
+        int newpos = 0;
+        newpos = newpos + nbCharsInFirstBlock;
+
+        //for each block in FAT that have to be written
+        for (int i = 0; i < blocksToWrite; i++)
+        {
+            string buffer = "";
+            //read the block to keep the content that is being overwritten
+            hd->ReadBlock(block, buffer);
+            overwritten += buffer;
+
+            //call hard drive's writeBlock method
+            hd->WriteBlock(block, temp);
+
+
+            //get the next block to which to write
+            int prevBlock = block;
+            block = FAT[block];
+            //if it is the end of file, get a new available block and give it to FAT
+            if (block == 0)
+            {
+                int b = hd->GetFirstAvailableMemorySpace();
+                FAT[block] = b;
+                FAT[b] = 0;
+                block = b;
+            }
+            int size = tampLecture.length();
+            if (size - newpos < 0)
+                break;
+            //get next 64 chars to write to file from tampLecture
+            temp = tampLecture.substr(newpos, newpos + BLOCK_SIZE >= size ? size - newpos : BLOCK_SIZE);
+            //update the position from where to write
+            newpos += BLOCK_SIZE;
+        }
+
+        //Append to file the content that was overwritten by write
+        block = files[nomFichier];
+        while (block != 0)
+        {
+            block = FAT[block]; //get ending block
+        }
+
+        //get the number of characters that can be written in last block
+        int nbCharsToWrite = hd->spaceLeftInBlock(block); //create function (TODO ANTOINE)
+        if (nbCharsToWrite == -1)
+        {
+            cout << "error in write(), appending not working!";
+            return;
+        }
+        int pos = 0;
+        //until everything was not appended, writeBlock
+        while (pos < overwritten.length())
+        {
+            //get chars to write to block corresponding to length that can ben written
+            string buffer = overwritten.substr(pos, nbCharsToWrite);
+            hd->WriteBlock(block, buffer);
+
+            //get the number of characters that can be written to next empty block
+            nbCharsToWrite = pos + BLOCK_SIZE < overwritten.length() ? BLOCK_SIZE : overwritten.length() - pos;
+            //then update position. The while condition will check if the word is finished being written
+            pos += nbCharsToWrite;
+        }
+    }
+
+    void deleteEOF(string nomFichier, int position)
+    {
+        int startBlock = floor(position / BLOCK_SIZE); //The block to start with
+        int index = files[nomFichier];
+        int pos = position % BLOCK_SIZE;
+
+        for (int i=0; i < startBlock; i++)
+        {
+            index = FAT[index];
+        }
+
+        bool first = true;
+        while (FAT[index] != 0)   //For every block
+        {
+            int nextBlockIndex = FAT[index];
+            if (first)
+            {
+                first = false;
+                string lecture = "";
+                hd->ReadBlock(index, lecture);
+                hd->WriteBlock(index, lecture.substr(pos, BLOCK_SIZE));
+            }
+            else
+            {
+                FAT[index] = 0;
+                hd->WriteBlock(index, "");
+            }
+            index = nextBlockIndex;
+        }
+    }
+
+    void printHD()
+    {
+        cout << "HardDrive" << endl;
+        for (int i = 0; i < BLOCK_COUNT; i++)
+        {
+            string lecture = "";
+            hd->ReadBlock(i, lecture);
+            if (lecture == "")
+                cout << " ";
+            else
+                cout << (lecture.length() < BLOCK_SIZE ? lecture[0] : (char)((int)lecture[0] - 32));
+        }
+        cout << endl;
+    }
+};
+
+int main(void)
+{
+
+
+    OS PatentedAwesomeTerminalOS;
+    int RFile, ROperation;
+    srand(time(NULL));
+    string theFile, tampon = "";
+    ofstream file(DISK_FILENAME);
+    file.close();
+
+    int count = 0;
+    for (int i = 0; i< 2000; i++)
+    {
+        RFile = rand () % 3 + 97;
+        ROperation = rand() %3 + 1;
+
+        theFile.push_back((char)RFile);
+        theFile.append(".txt");
+        int nbrChar = rand() % 256 + 1;
+
+        switch (ROperation)
+        {
+        case 1: //READ
+            PatentedAwesomeTerminalOS.read(theFile, rand() % 256, nbrChar, tampon);
+            break;
+        case 2: //WRITE
+            for (int j=0; j<nbrChar; j++)
+                tampon.push_back((char)RFile);
+            PatentedAwesomeTerminalOS.write(theFile, rand() % 256, nbrChar, tampon);
+            break;
+        case 3: //DELETEEOF
+            PatentedAwesomeTerminalOS.deleteEOF(theFile, rand() % 256);
+            break;
+        }
+
+        theFile = "";
+
+        if (count == 100)
+        {
+            PatentedAwesomeTerminalOS.printHD();
+            count = 0;
+        }
+        else
+            count++;
+    }
+
+    return 0;
+}
